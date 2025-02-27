2a2773a0fa0c44a1208606422f08bd61dc0b18ce
Velan <119405038+VELANDHANANJAYAN@users.noreply.github.com>
2025-02-27 19:13:34 +0530

    Time: 1 ms (22.98%) | Memory: 42.1 MB (45.28%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public void sortColors(int[] nums) {
+        for(int i=0;i<nums.length;i++)
+        {
+            Arrays.sort(nums);
+        }
+    }
+}
