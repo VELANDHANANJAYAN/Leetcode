dd0a87d03fc20abab8ecd2e08013fa0b37bdf91a
Velan <119405038+VELANDHANANJAYAN@users.noreply.github.com>
2025-03-18 05:20:48 +0530

    Time: 0 ms (100.00%) | Memory: 50.4 MB (40.43%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,22 +1,23 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        // HashMap<Integer,Integer> map=new HashMap<>();
-        // for(int i=0;i<nums.length;i++)
-        // {
-        //     int value=map.getOrDefault(nums[i],0);
-        //     map.put(nums[i],value+1);
-        // }
-        // for(Map.Entry<Integer,Integer> it:map.entrySet()){
-        //     if(it.getValue()==1){
-        //         return it.getKey();
-        //     }
-        // }
-        // return 0;
-        int xor=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            xor=xor^nums[i];
+        
+        int low = 0, high = nums.length - 1;
+        
+        while (low < high) {
+            int mid =low+(high-low) / 2;
+            
+            // Make sure mid is even
+            if (mid % 2 == 1) mid--;
+
+            if (nums[mid] == nums[mid + 1]) {
+                // Unique element is in the right half
+                low = mid + 2;
+            } else {
+                // Unique element is in the left half (including mid)
+                high = mid;
+            }
         }
-        return xor;
+        
+        return nums[low]; // or nums[high], both are same here
     }
 }
